69aa408959db6ef9e1946f2793cc35c3c98425e0
sisa95 <ponyesisa@gmail.com>
2021-11-12 00:06:51 +0200

    configured travisCI

--- FruitsBasket.js
+++ FruitsBasket.js
@@ -25,47 +25,37 @@ module.exports = function Fruits_Basket(pool) {
 
                 arr.push(element.fruit_type)
             });
-            console.log(arr)
             return arr
 
-        } catch (error) {
-            console.log(error)
+        } catch (error) { console.log(error)
         }
     }
 
-    async function getBasketTotalPrice(fruit_type, unit_price, quantity) {
+    async function getBasketTotalPrice() {
         try {
             let totalPrice = await pool.query('SELECT fruit_type, quantity * unit_price  AS total_price FROM fruit_basket')
-            console.log(totalPrice.rows)
             return totalPrice.rows
 
-        } catch (error) {
-            console.log(error)
+        } catch (error) { console.log(error)
         }
     }
 
     async function updateFruitBasket(fruit_type) {
        try{
-            await pool.query('select * from fruit_basket')
             await pool.query(`UPDATE fruit_basket SET quantity = (quantity + 1) WHERE fruit_type = $1`, [fruit_type])
             let updateBasket = await pool.query('select quantity from fruit_basket where fruit_type = $1',[fruit_type])
-            console.log( updateBasket.rows)
-
             return updateBasket.rows
 
-        } catch (error) {
-            console.log(error)
+        } catch (error) {  console.log(error)
         }
     }
 
     async function getTotal() {
         try {
             let x = await pool.query('SELECT SUM(unit_price) FROM fruit_basket') 
-            console.log(x.rows)
             return x.rows
 
-        } catch (error) {
-            console.log(error)
+        } catch (error) { console.log(error)
         }
     }
 
@@ -76,6 +66,5 @@ module.exports = function Fruits_Basket(pool) {
         getBasketTotalPrice,
         updateFruitBasket,
         getTotal
-
     }
 }
